8fcd763101a42286e83f9946b3ada3ef19a7921c
jachipana <chipanavillcajoel123@gmail.com>
2023-12-11 09:04:45 -0400

    se agrego el primer y segundo ejercicio

--- src/views/PrimerEjercicio.vue
+++ src/views/PrimerEjercicio.vue
@@ -1,22 +1,44 @@
 <template>
-    <h1>Hello primer ejercicio</h1>
-    <button @click="mostrarID">generar</button>
+    <h1>Primer Ejercicio</h1>
+    <h1>LISTA DE USUARIOS</h1>
+    <div>
+        Cantidad de imágenes
+        <input type="number" v-model="cantidad">
+    </div>
+    <br>
+    <div>
+        <button @click="cargarImagenes">CARGAR IMÁGENES</button>
+    </div>
+    <div v-for="(user, index) in usuarios" :key="index">
+        {{ index }}
+        <img :src="user.picture.large">
+    </div>
 </template>
-
+  
 <script>
-import { v4 as uuidv4 } from 'uuid'
+import { ref } from 'vue';
+
 export default {
     name: 'PrimerEjercicio',
     setup() {
-        const generarID = () => {
-            return uuidv4();
-        }
-        const mostrarID = () => {
-            console.log(generarID())
-        }
-        return { mostrarID }
-    }
-}
-</script>
+        const cantidad = ref(0);
+        const usuarios = ref([]);
 
+        const cargarImagenes = async () => {
+            try {
+                const respuesta = await fetch(`https://randomuser.me/api/?results=${cantidad.value}`);
+                const data = await respuesta.json();
+                usuarios.value = data.results;
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+
+        return { cantidad, cargarImagenes, usuarios };
+    },
+};
+</script>
+  
 <style scoped></style>
+  
--- src/views/SegundoEjercicio.vue
+++ src/views/SegundoEjercicio.vue
@@ -1,12 +1,81 @@
 <template>
-    <h1>Hello segundo ejercicio</h1>
+    <h1>Segundo ejercicio</h1>
+    <div>
+        INGRESE N° DE FILAS
+        <input v-model="numFilas" type="number" />
+    </div>
+    <br />
+    <div>
+        <button @click="generarMatriz">GENERAR</button>
+    </div>
+    <br />
+    <div>
+        <button @click="sumarFilas">SUMAR LOS ELEMENTOS DE LAS FILAS</button>
+    </div>
+    <div class="contenedor">
+        <div class="fila" v-for="(fila, nroFila) in matriz" :key="nroFila">
+            {{ nroFila }}
+            <div class="numerito" v-for="(valor, nroColumna) in fila" :key="nroColumna">
+                {{ valor }}
+            </div>
+        </div>
+    </div>
 </template>
-
+  
 <script>
+import { ref } from 'vue';
+
 export default {
     name: 'SegundoEjercicio',
-    setup() { }
-}
+    setup() {
+        const numFilas = ref(0);
+        const matriz = ref([]);
+        const generarMatriz = () => {
+            const m = parseInt(numFilas.value);
+            matriz.value = Array.from({ length: m }, () =>
+                Array(3).fill(0).map(() => Math.floor(Math.random() * 10) + 1)
+            );
+        };
+        const sumarFilas = () => {
+            for (const fila of matriz.value) {
+                const sumaFila = fila.reduce((x, valor) => x + valor, 0);
+                console.log("sumFil:" + sumaFila);
+            }
+        };
+        return {
+            numFilas,
+            matriz,
+            generarMatriz,
+            sumarFilas,
+        };
+    },
+};
 </script>
+  
+<style scoped>
+.contenedor {
+    background-color: red;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 
-<style scoped></style>
+.fila {
+    display: flex;
+    margin: 5px;
+}
+
+.numerito {
+    background-color: black;
+    color: white;
+    width: 60px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid;
+    border-radius: 5px;
+    margin: 2px;
+}
+</style>
+  
