415a58895044576be657619156fe840156e3fa2e
jachipana <chipanavillcajoel123@gmail.com>
2023-12-10 23:06:49 -0400

    se agrego un boton que sirve para generar IDs

---
+++ src/views/PrimerEjercicio.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Hello primer ejercicio</h1>
+    <button @click="mostrarID">generar</button>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid'
+export default {
+    name: 'PrimerEjercicio',
+    setup() {
+        const generarID = () => {
+            return uuidv4();
+        }
+        const mostrarID = () => {
+            console.log(generarID())
+        }
+        return { mostrarID }
+    }
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/SegundoEjercicio.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Hello segundo ejercicio</h1>
+</template>
+
+<script>
+export default {
+    name: 'SegundoEjercicio',
+    setup() { }
+}
+</script>
+
+<style scoped></style>
